1e6c315eea5152dc674e8a0b87980f017787d3c0
pengdst <pengkuh.dst@gmail.com>
2021-11-19 11:07:18 +0700

    fix update transaksi

--- application/controllers/api/Transaksi.php
+++ application/controllers/api/Transaksi.php
@@ -103,9 +103,7 @@ class Transaksi extends REST_Controller
 	public function index_put($id)
 	{
 		if ($this->checkToken() !== true) return $this->response($this->checkToken(), REST_Controller::HTTP_UNAUTHORIZED);
-		$params = [
-			'tanggal' => date("Y-m-d H:i:s"),
-		];
+		$params = [];
 		if ($this->put('admin_id') != null) {
 			if ($this->Admin->getById($this->put('admin_id'))) {
 				return $this->response([
@@ -130,6 +128,8 @@ class Transaksi extends REST_Controller
 			'message' => "Tidak ada perubahan data"
 		], REST_Controller::HTTP_NOT_MODIFIED);
 
+		$params['tanggal'] = date("Y-m-d H:i:s");
+
 		$data = $this->Transaksi->update($id, $params);
 
 		if ($data === null) return $this->response([
@@ -145,6 +145,140 @@ class Transaksi extends REST_Controller
 	}
 
 	public function index_delete($id)
+	{
+		if ($this->checkToken() !== true) return $this->response($this->checkToken(), REST_Controller::HTTP_UNAUTHORIZED);
+
+		if ($this->Transaksi->getById($id) == null) return $this->response([
+			'success' => false,
+			'message' => "Data Tidak Ditemukan",
+		], REST_Controller::HTTP_NOT_FOUND);
+
+		if (!$this->Transaksi->delete($id)) {
+			return $this->response([
+				'success' => false,
+				'message' => "Data Gagal Dihapus",
+			], REST_Controller::HTTP_UNPROCESSABLE_ENTITY);
+		}
+
+		return $this->response([
+			'success' => true,
+			'message' => "Data Transaksi berhasil dihapus",
+			'data' => $this->Transaksi->all()
+		], REST_Controller::HTTP_OK);
+	}
+
+	public function item_get($id = null)
+	{
+		if ($this->checkToken() !== true) return $this->response($this->checkToken(), REST_Controller::HTTP_UNAUTHORIZED);
+		$data = $id === null ? $this->Transaksi->all() : $this->Transaksi->getById($id);
+
+		if ($data === null) {
+			return $this->response([
+				'success' => false,
+				'message' => "Data tidak ditemukan"
+			], REST_Controller::HTTP_NOT_FOUND);
+		}
+
+		return $this->response([
+			'success' => true,
+			'message' => "Data berhasil ditampilkan",
+			'data' => $data
+		], REST_Controller::HTTP_OK);
+	}
+
+	public function item_post()
+	{
+		if ($this->checkToken() !== true) return $this->response($this->checkToken(), REST_Controller::HTTP_UNAUTHORIZED);
+		if ($this->post('admin_id') == null) {
+			return $this->response([
+				'success' => false,
+				'message' => "Field admin_id harus diisi",
+			], REST_Controller::HTTP_UNPROCESSABLE_ENTITY);
+		}
+		if ($this->Admin->getById($this->post('admin_id'))==null) {
+			return $this->response([
+				'success' => false,
+				'message' => "Admin tidak ditemukan",
+			], REST_Controller::HTTP_NOT_FOUND);
+		}
+		if ($this->post('total') == null) {
+			return $this->response([
+				'success' => false,
+				'message' => "Field total harus diisi",
+			], REST_Controller::HTTP_UNPROCESSABLE_ENTITY);
+		}
+		if (!is_numeric($this->post('total'))) {
+			return $this->response([
+				'success' => false,
+				'message' => "Field total harus numeric",
+			], REST_Controller::HTTP_UNPROCESSABLE_ENTITY);
+		}
+
+		$params = [
+			'admin_id' => $this->post('admin_id'),
+			'total' => $this->post('total'),
+			'tanggal' => date("Y-m-d H:i:s"),
+		];
+		if (($id = $this->Transaksi->create($params)) === null) {
+			return $this->response([
+				'success' => false,
+				'message' => "Insert Data Gagal",
+			], REST_Controller::HTTP_UNPROCESSABLE_ENTITY);
+		}
+		$data = $data = $this->Transaksi->getById($id);
+
+		return $this->response([
+			'success' => true,
+			'message' => "Data Transaksi berhasil ditambahkan",
+			'data' => $data
+		], REST_Controller::HTTP_OK);
+	}
+
+	public function item_put($id)
+	{
+		if ($this->checkToken() !== true) return $this->response($this->checkToken(), REST_Controller::HTTP_UNAUTHORIZED);
+		$params = [];
+		if ($this->put('admin_id') != null) {
+			if ($this->Admin->getById($this->put('admin_id'))) {
+				return $this->response([
+					'success' => false,
+					'message' => "Admin tidak ditemukan",
+				], REST_Controller::HTTP_NOT_FOUND);
+			}
+			$params['admin_id'] = $this->put('admin_id');
+		}
+		if ($this->put('total') != null) {
+			if (!is_numeric($this->put('total'))) {
+				return $this->response([
+					'success' => false,
+					'message' => "Field total harus numeric",
+				], REST_Controller::HTTP_UNPROCESSABLE_ENTITY);
+			}
+			$params['total'] = $this->put('total');
+		}
+
+		if (empty($params)) return $this->response([
+			'success' => false,
+			'message' => "Tidak ada perubahan data"
+		], REST_Controller::HTTP_NOT_MODIFIED);
+
+		$params['tanggal'] = date("Y-m-d H:i:s");
+
+		$data = $this->Transaksi->update($id, $params);
+
+		if ($data === null) return $this->response([
+			'success' => false,
+			'message' => "Data tidak ditemukan"
+		], REST_Controller::HTTP_NOT_FOUND);
+
+		return $this->response([
+			'success' => true,
+			'message' => "Data Transaksi berhasil diupdate",
+			'data' => $data
+		], REST_Controller::HTTP_OK);
+	}
+
+	public function item_delete($id)
 	{
 		if ($this->checkToken() !== true) return $this->response($this->checkToken(), REST_Controller::HTTP_UNAUTHORIZED);
 
