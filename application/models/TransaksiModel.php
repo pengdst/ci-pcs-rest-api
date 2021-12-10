b4b1674bda8132c06d2dbf0a80cf098b658338f6
pengdst <pengkuh.dst@gmail.com>
2021-12-10 18:17:53 +0700

    add field date_in_milis

--- application/models/TransaksiModel.php
+++ application/models/TransaksiModel.php
@@ -5,4 +5,15 @@ class TransaksiModel extends CyModel {
 
     protected $tableName = "transaksi";
     
+    public function all()
+    {
+        $results = parent::all();
+        foreach($results as $key => $transaction){
+            // $transactions[$key]['answers'] = $answers_model->get_answers_by_transaction_id($transaction['transaction_id']);
+            $transaction->date_in_milis = strtotime($transaction->tanggal);
+            $results[$key] = $transaction;
+        }
+        return $results;
+    }
+    
 }
